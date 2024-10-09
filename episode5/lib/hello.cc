88eca533333b9f18d7e01a95a0a03906896cc897
pym1900 <pym27769@gmail.com>
2024-10-09 15:52:57 +0800

    Add episode 5 content

---
+++ episode5/lib/hello.cc
@@ -0,0 +1,5 @@
+#include "hello.hpp"
+
+void HelloFunc() {
+    std::cout << "Hello World\n";
+}
---
+++ episode5/lib/hello.hpp
@@ -0,0 +1,8 @@
+#ifndef __HELLO_HPP
+#define __HELLO_HPP
+
+#include <iostream>
+
+void HelloFunc();
+
+#endif
